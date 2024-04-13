13379d22748c4688dfcd0e1e08b9e2e124f2b30f
SiddharthKrSingh <116479873+SiddharthKrSingh@users.noreply.github.com>
2024-04-13 15:58:51 +0530

    data ingestion added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -62,7 +62,7 @@ class ConfigurationManager:
     #     training = self.config.training
     #     prepare_base_model = self.config.prepare_base_model
     #     params = self.params
-    #     training_data = os.path.join(self.config.data_ingestion.unzip_dir, "brain_tumor_dataset")
+    #     training_data = os.path.join(self.config.data_ingestion.unzip_dir, "pne_dataset")
     #     create_directories([
     #         Path(training.root_dir)
     #     ])
