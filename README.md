438b814dafc6c86c186fb3cf7322f70a2f352fa7
SiddharthKrSingh <116479873+SiddharthKrSingh@users.noreply.github.com>
2024-04-13 19:16:53 +0530

    model evaluation added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Pneumonia-Prediction-Model
+
+<!-- MLFLOW_TRACKING_URI=https://dagshub.com/siddharthsingh590/Pneumonia-Prediction-Model.mlflow \
+MLFLOW_TRACKING_USERNAME=siddharthsingh590 \
+MLFLOW_TRACKING_PASSWORD=45d318899d6dfacf2f0daf38350dade42f59cc95 \
+python script.py -->
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -82,13 +82,13 @@ class ConfigurationManager:
     
 
 
-    # def get_evaluation_config(self) -> EvaluationConfig:
-    #     eval_config = EvaluationConfig(
-    #         path_of_model="artifacts/training/model.h5",
-    #         training_data="artifacts/data_ingestion/pne_dataset",
-    #         mlflow_uri="https://dagshub.com/siddharthsingh590/Brain-Tumor-Classification.mlflow",
-    #         all_params=self.params,
-    #         params_image_size=self.params.IMAGE_SIZE,
-    #         params_batch_size=self.params.BATCH_SIZE
-    #     )
-    #     return eval_config
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/pne_dataset",
+            mlflow_uri="https://dagshub.com/siddharthsingh590/Pneumonia-Prediction-Model.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
